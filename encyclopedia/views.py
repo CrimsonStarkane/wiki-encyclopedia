426839cc3573d970af06c34e0b6745d7c6ea58f8
Janriz Libres <leonmadrik11037@gmail.com>
2023-04-22 11:51:59 +0800

    modified urls

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    print(entry)
+    content = util.get_entry(entry)
+    print(content)
+
+    if content == None:
+        return render(request, "encyclopedia/notfound.html")
+
+    return render(request, "encyclopedia/entry.html", {
+        "content": content
+    })
